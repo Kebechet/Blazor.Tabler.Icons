a1e0347df80f4179cef2808e9ddd9d0f06d14d85
Kebechet <samosidor@gmail.com>
2026-01-06 19:02:32 +0100

    Added tabler icon functionality

---
+++ src/Blazor.Icons.Tabler/wwwroot/Blazor.Icons.Tabler.lib.module.js
@@ -0,0 +1,11 @@
+export function beforeWebStart(options, extensions) {
+	beforeStart(options, extensions);
+}
+
+export function beforeStart(options, extensions) {
+	var element = document.createElement('link');
+	element.href = "_content/Blazor.Icons.Tabler/tabler-icons.min.css";
+	element.rel = "stylesheet";
+	element.async = false;
+	document.head.appendChild(element);
+}
